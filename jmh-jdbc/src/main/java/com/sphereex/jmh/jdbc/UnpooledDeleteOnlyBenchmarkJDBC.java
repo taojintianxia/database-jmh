af69e5143da1e982602a80d7be2ecd26ec24f06d
wsm12138 <1473602179@qq.com>
2022-05-05 15:14:48 +0800

    add case for jmh-jdbc

---
+++ jmh-jdbc/src/main/java/com/sphereex/jmh/jdbc/UnpooledDeleteOnlyBenchmarkJDBC.java
@@ -0,0 +1,9 @@
+package com.sphereex.jmh.jdbc;
+
+import com.sphereex.jmh.jdbc.connection.UnpooledJDBCConnectionProvider;
+
+public class UnpooledDeleteOnlyBenchmarkJDBC extends UnpooledDeleteOnlyBenchmarkBase implements UnpooledJDBCConnectionProvider {
+}
+
+
+
---
+++ jmh-jdbc/src/main/java/com/sphereex/jmh/jdbc/UnpooledUpdateIndexBenchmarkJDBC.java
@@ -0,0 +1,6 @@
+package com.sphereex.jmh.jdbc;
+
+import com.sphereex.jmh.jdbc.connection.UnpooledJDBCConnectionProvider;
+
+public class UnpooledUpdateIndexBenchmarkJDBC  extends UnpooledIndexUpdateOnlyBenchmarkBase implements UnpooledJDBCConnectionProvider {
+}
